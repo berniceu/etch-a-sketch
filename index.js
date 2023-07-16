ae309455583282ae07aeaac1c2766d85e0d9fe3f
berniceu <berniceuwituze@gmail.com>
2023-07-16 20:04:03 +0200

    create rows and cells divs and append to container div

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-const container = document.createElement('div');
+const container = document.getElementsById('#container');
+let rows = document.createElement('div');
+let cells = document.createElement('div');
+container.appendChild('rows');
+container.appendChild('cells');
