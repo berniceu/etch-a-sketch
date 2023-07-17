1997bd535054f4ddf53f1379ca70e5bb40459210
berniceu <berniceuwituze@gmail.com>
2023-07-17 19:51:53 +0200

    update index.html file

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const container = document.getElementsById('#container');
 let rows = document.createElement('div');
 let cells = document.createElement('div');
+rows.classList.add('rows');
+cells.classList.add('cells');
 container.appendChild('rows');
 container.appendChild('cells');
+
+function makeRows() {
+    
+}
